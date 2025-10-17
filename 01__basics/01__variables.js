19351fcc6a6d95b3b1fc54f9e09e83f2517ecb83
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-17 16:23:55 +0000

    practice of var let const

---
+++ 01__basics/01__variables.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail = "daniyal@gmail.com"
+var accountPassword = "112233"
+
+accountCity = "islamabad"
+
+accountEmail = "hehhs@gmail.com"
+accountPassword = "2121353"
+accountCity = "kashmir"
+
+//accountId = 3  constats are not allowed to change 
+console.log(accountId);
+console.table([accountEmail,accountPassword,accountId,accountCity]);
