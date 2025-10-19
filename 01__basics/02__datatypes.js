1de1cc21bbc22c9eb9b91eaa8574b54b4afa2b54
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-19 07:04:37 +0000

    data types in js

---
+++ 01__basics/02__datatypes.js
@@ -0,0 +1,9 @@
+// there are 8 types of data types in javascript which are mostly used//
+// 1) Number
+// 2) String
+// 3) Boolean
+// 4) Bigint
+// 5) Null
+// 6) Undefined
+// 7) Symbol
+// 8) Object 
