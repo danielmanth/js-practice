dcb2a818ce7cf1c1d249afdb5a59de86560b06a5
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-19 08:12:31 +0000

    datatypes

--- 01__basics/02__datatypes.js
+++ 01__basics/02__datatypes.js
@@ -7,3 +7,14 @@
 // 6) Undefined
 // 7) Symbol
 // 8) Object 
+
+// 1) Numbers are genrally defined Exp , 1,2,3,4
+// 2) String can be used with single or double qouted (my prefrence for string is "" double qouted)
+// 3) Boolean are too simple they contain only 2 values (True and False)
+// 4) Bigint are used when you have some big numbers like Exp =(1234567890123456789012345678901234567890n) and when you put (n) after the numbers it will considered as a bigint value.
+
+
+
+
+
+console.log(typeof "daniyal")
