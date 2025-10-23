fdf1e217183792e266279fa09495245ca1a7dc86
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-23 18:23:12 +0000

    conversiondatatypes.js

--- 01__basics/03__conversiondatatypes.js
+++ 01__basics/03__conversiondatatypes.js
@@ -1,2 +1,6 @@
 // all we here disscus about the conversion of data types like how we can convert any value to others.
 
+let str1 = 30
+let str2 = 30
+let str3 = str1 + str2
+console.log(`value of str${str3})`)
