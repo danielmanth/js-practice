5226de049907b363ee34bf53d391fcc7dfe899df
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-24 17:19:37 +0000

    Stack&Heap

--- 01__basics/04__Stack&Heap.js
+++ 01__basics/04__Stack&Heap.js
@@ -3,3 +3,8 @@
 // Stack memory and Heap memory 
 // Stack Memory ( premitive )
 // Heap Memory ( Non- premitive )
+
+// What is Stack Memory ?
+// In stack memory when you declare variable  you will get the copy of it.
+// What is Heap Memory ?
+// In heap memory when you declare variable you will get the refrence of its orignal(means your changes directly apply on it).
