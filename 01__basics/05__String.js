7b8121e8556f25fe4e05187ebde45e34030fddca
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-26 19:35:15 +0000

    05__Strings.js

--- 01__basics/05__String.js
+++ 01__basics/05__String.js
@@ -1,3 +1,11 @@
 // String is a text that can be assigned or stored in variable.
 // String can be written in single qoute ('), Or also can be write in double qoute ("").
-//
+// How to find the data-type of String.
+//const x = session 1
+// String Concatination means you add two or more strings in one line 
+const x = 1
+const str = "Daniyal Mehmood"
+const str1 = " Practice session "
+console.log(str + str1);
+
+// Here we see about the Template Strings with backticks-``
