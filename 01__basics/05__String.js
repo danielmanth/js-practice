e71c11663deadbbe9976eb529569e983b4fb88ab
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-27 18:27:36 +0000

    String

--- 01__basics/05__String.js
+++ 01__basics/05__String.js
@@ -1,9 +1,10 @@
-const str = 'programming for beginners " youtube channel" '
-// const str1 = " youtube channel"
+const x = 1
+const str = `"programming for beginners"  'youtube channel' - ${x}`
+//const str1 = " youtube channel"
 console.log(str);
 // Here we see about the Quotes inside a Quote in string.
 
 
 //Also remember we cannot provide double quotes in double quotes & not single quotes in single quotes.
 
-// If you want double and single quotes in the same code of line use template strings with backticks.
+// If you want double and single quotes in the same code of line use template strings with backticks and also it will help you to add any new number you want to add in the string.
