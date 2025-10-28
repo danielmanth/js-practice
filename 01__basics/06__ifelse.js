51d37a4e12146e205254b1e128e8f17f5fb5f501
Daniyal Mehmood <111997654+danielmanth@users.noreply.github.com>
2025-10-28 18:24:14 +0000

    ifelse

---
+++ 01__basics/06__ifelse.js
@@ -0,0 +1,9 @@
+// Very often it happens when we write a code we need to use different code for different statments.
+// Here we use conditional Statments 
+
+// there are some types of Conditional Statments 
+// Else-If , Else , If .
+// these are some of them and here we explain about them all.
+// use if statment to execute a block of code if the condition is true.
+// use else statment to execute a block of code if the same condition is false.
+// use else-if statment to execute a block of code if the 
